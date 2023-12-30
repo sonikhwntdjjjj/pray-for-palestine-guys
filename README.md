070628976aa2ec0a6ece5b6bb47e6ceb57b53539
sonikhhhhh <153660624+sonikhwntdjjjj@users.noreply.github.com>
2023-12-30 19:44:13 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# PalestineWillBeFree
+A simple responsive website which navigate to a fund raiser for Palestine.
+
+#Solidariti untuk rakyat Palestin
---
+++ script.js
@@ -0,0 +1,5 @@
+const navigateButton = document.getElementById('navigateButton');
+
+navigateButton.addEventListener('click', () => {
+    window.location.href = 'https://jommenyumbang.com/ms/kempen/bantuan-kecemasan-palestin';
+});
